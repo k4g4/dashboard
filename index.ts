57fd513b3934f42db96caab20085215b2c92cb21
Andres Dejesus <andresdejesus123@gmail.com>
2024-03-25 22:20:01 -0500

    attempting to figure out bun server

--- index.ts
+++ index.ts
@@ -1,1 +1,39 @@
-console.log("Hello via Bun!");
+const development = Bun.env.DEVELOPMENT === 'true'
+
+let files: Map<string, string>
+if (development) {
+    files = new Map([["/", "index.htm"]])
+} else {
+    const { success, logs, outputs } = await Bun.build({
+        entrypoints: ["index.htm"],
+        outdir: Bun.env.OUT_DIR,
+    })
+
+    if (!success) {
+        throw logs.toString()
+    }
+    files = new Map([["/", require(outputs[0].path)]])
+}
+
+async function fetch(req: Request) {
+    const { method, url } = req
+    const { pathname, searchParams } = new URL(url)
+
+    if (method === 'GET') {
+        const page = files.get(pathname)
+        if (!page) {
+            return new Response(`404 - File not found`, { status: 404 })
+        }
+        return new Response(Bun.file(page))
+    }
+
+    return new Response(`404 - File not found`, { status: 404 })
+}
+
+const server = Bun.serve({
+    fetch,
+    development,
+    port: Bun.env.PORT,
+})
+
+console.log(`listening on ${server.url}`)
